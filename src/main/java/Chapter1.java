7212e5155dcff65aceeb09ed91bef98904ea079e
Wynne Tran <wynnetran@gmail.com>
2019-03-19 17:18:15 -0700

    Add isSubstring for stringRotation problem

--- src/main/java/Chapter1.java
+++ src/main/java/Chapter1.java
@@ -133,4 +133,15 @@ public class Chapter1 {
     return false;
   }
 
+  /**
+   * Checks if s2 is a substring of s1.
+   *
+   * @param s1 string
+   * @param s2 string
+   * @return true if s2 is a substring of s1
+   */
+  private boolean isSubstring(String s1, String s2) {
+    return s1.contains(s2);
+  }
+
 }
