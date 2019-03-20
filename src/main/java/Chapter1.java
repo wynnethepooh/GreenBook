7d5bcace6a2dae67099a2dd012be6db6bd58a5d8
Wynne Tran <wynnetran@gmail.com>
2019-03-19 17:21:47 -0700

    Implement stringRotation

--- src/main/java/Chapter1.java
+++ src/main/java/Chapter1.java
@@ -331,6 +331,10 @@ public class Chapter1 {
    * @return true if s1 is a rotation of s2
    */
   public boolean stringRotation(String s1, String s2) {
+    if (s1.length() == s2.length() && s1.length() > 0) {
+      String s1s1 = s1 + s1;
+      return isSubstring(s1s1, s2);
+    }
     return false;
   }
 
