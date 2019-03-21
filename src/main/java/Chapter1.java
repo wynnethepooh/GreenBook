aea64fec90b58112c612654228dca8743600ed7e
Wynne Tran <wynnetran@gmail.com>
2019-03-21 14:30:36 -0700

    Modify zeroMatrix to use boolean arrays instead of sets

--- src/main/java/Chapter1.java
+++ src/main/java/Chapter1.java
@@ -303,21 +303,21 @@ public class Chapter1 {
    * @param matrix MxN matrix
    */
   public void zeroMatrix(int[][] matrix) {
-    Set<Integer> zeroRows = new HashSet<>();
-    Set<Integer> zeroCols = new HashSet<>();
+    boolean[] zeroRows = new boolean[matrix.length];
+    boolean[] zeroCols = new boolean[matrix[0].length];
 
     for (int row = 0; row < matrix.length; row++) {
       for (int col = 0; col < matrix[row].length; col++) {
         if (matrix[row][col] == 0) {
-          zeroRows.add(row);
-          zeroCols.add(col);
+          zeroRows[row] = true;
+          zeroCols[col] = true;
         }
       }
     }
 
     for (int row = 0; row < matrix.length; row++) {
       for (int col = 0; col < matrix[row].length; col++) {
-        if (zeroRows.contains(row) || zeroCols.contains(col)) {
+        if (zeroRows[row] || zeroCols[col]) {
           matrix[row][col] = 0;
         }
       }
