5f64bb9518846881bafa55319df583ea1b9609fa
Wynne Tran <wynnetran@gmail.com>
2019-04-18 14:08:15 -0700

    Add test for route between nodes in graph

--- src/main/java/util/GraphNode.java
+++ src/main/java/util/GraphNode.java
@@ -1,10 +1,22 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphNode {
   public int data;
-  public GraphNode[] children;
+  public List<GraphNode> adjacent;
+  public State state;
 
   public GraphNode(int d) {
     data = d;
+    state = State.Unvisited;
+    adjacent = new ArrayList<>();
   }
+
+  public void addAdjacent(GraphNode node) {
+    adjacent.add(node);
+  }
+
+  public enum State { Unvisited, Visited }
 }
